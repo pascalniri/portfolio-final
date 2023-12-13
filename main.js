4410b0f3cb8b1f77e2b3f583a850ec10451f0b3c
pascalniri <pascalniri@gmail.com>
2023-12-13 20:04:58 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const myDiv =document.getElementById('myDiv');
+const myKintu = document.createElement('h1');
+const myContent=document.createTextNode('Niringiyimana Pascal');
+const par = document.getElementById('myPar');
+const newPar = document.createTextNode('This is my profle');
+par.appendChild(newPar);
+const pic = document.get
+// myKintu.appendChild(myContent);
+// myDiv.appendChild(myKintu);
+
